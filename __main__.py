2d248f8ef984731b47e1c61aa53d654fa93ff19a
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2020-01-28 09:27:56 +0100

    Update to support Python programs

--- __main__.py
+++ __main__.py
@@ -1,44 +1,36 @@
 import json
 import subprocess
 import sys
+import tempfile
+import venv
 from pathlib import Path
 from typing import Tuple, Dict
 
-import click as click
+import click
 import numpy as np
 
 VALID_RESULTS = {
-    'img_000.jpg': np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1]),
-    'img_001.jpg': np.array([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]),
-    'img_002.jpg': np.array([3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]),
-    'img_003.jpg': np.array([1, 0, 0, 1, 1, 0, 2, 2, 2, 0, 1, 1, 0, 1, 1]),
-    'img_004.jpg': np.array([4, 4, 2, 7, 5, 5, 2, 2, 2, 0, 1, 1, 0, 1, 1]),
-    'img_005.jpg': np.array([4, 4, 2, 7, 5, 5, 2, 2, 2, 0, 1, 1, 3, 3, 3]),
-    'img_006.jpg': np.array([4, 4, 2, 7, 5, 5, 2, 2, 2, 0, 1, 1, 3, 3, 3]),
-    'img_007.jpg': np.array([5, 5, 6, 4, 4, 5, 2, 1, 3, 3, 2, 2, 0, 0, 0]),
-    'img_008.jpg': np.array([5, 5, 6, 4, 4, 5, 2, 3, 3, 5, 3, 5, 1, 0, 0]),
-    'img_009.jpg': np.array([6, 7, 6, 9, 8, 8, 3, 3, 3, 5, 3, 5, 3, 3, 3]),
-    'img_010.jpg': np.array([2, 1, 0, 4, 3, 2, 3, 2, 1, 1, 1, 1, 1, 0, 4]),
-    'img_011.jpg': np.array([2, 3, 4, 4, 3, 3, 6, 3, 2, 1, 1, 2, 2, 3, 5]),
-    'img_012.jpg': np.array([2, 3, 4, 4, 3, 3, 6, 3, 2, 1, 1, 2, 2, 3, 5]),
-    'img_013.jpg': np.array([2, 3, 4, 4, 3, 8, 6, 3, 2, 1, 2, 6, 4, 3, 5]),
-    'img_014.jpg': np.array([2, 3, 4, 2, 3, 6, 5, 3, 3, 3, 2, 4, 4, 2, 3]),
+    'img_009.jpg': np.array([28, 16, 21, 34, 23, 16]),
+    'img_010.jpg': np.array([28, 16, 21, 34, 23, 16]),
 }
 
 
 @click.command()
 @click.argument('applications_directory', type=click.Path(exists=True, file_okay=False))
+@click.argument('input_file', type=click.Path(exists=True, dir_okay=False))
 @click.argument('images_directory', type=click.Path(exists=True, file_okay=False))
 @click.argument('output_directory', type=click.Path(exists=True, file_okay=False))
 @click.option('--no-run', is_flag=True)
 @click.option('--no-compute', is_flag=True)
-def main(applications_directory: str, images_directory: str, output_directory: str, no_run: bool, no_compute: bool):
+def main(applications_directory: str, input_file: str, images_directory: str, output_directory: str, no_run: bool,
+         no_compute: bool):
     applications_directory = Path(applications_directory)
+    input_file = Path(input_file)
     images_directory = Path(images_directory)
     output_directory = Path(output_directory)
 
     if not no_run:
-        states = run_applications(applications_directory, images_directory, output_directory)
+        states = run_applications(applications_directory, input_file, images_directory, output_directory)
         with open(output_directory / 'states.json', 'w') as states_file:
             json.dump(states, states_file)
 
@@ -48,12 +40,13 @@ def main(applications_directory: str, images_directory: str, output_directory: s
             json.dump(results, results_file)
 
 
-def run_applications(applications_directory: Path, images_directory: Path, output_directory: Path) -> Dict[str, str]:
+def run_applications(applications_directory: Path, input_file: Path, images_directory: Path,
+                     output_directory: Path) -> Dict[str, str]:
     states = {}
     for applications_directory_entry in applications_directory.iterdir():
         if applications_directory_entry.is_dir():
-            student_name, status = process_application_directory(applications_directory_entry, images_directory,
-                                                                 output_directory)
+            student_name, status = process_application_directory(applications_directory_entry, input_file,
+                                                                 images_directory, output_directory)
             states[student_name] = status
 
     return states
@@ -62,42 +55,57 @@ def run_applications(applications_directory: Path, images_directory: Path, outpu
 def compute_results(output_directory: Path) -> Dict[str, float]:
     results = {}
     for student_output_directory in output_directory.iterdir():
-        results_file_path: Path = student_output_directory / 'results.txt'
-        if results_file_path.exists():
-            try:
-                images_scores_sum = 0.0
-                number_of_lines = 0
-                with open(results_file_path) as results_file:
-                    for line in results_file:
-                        number_of_lines += 1
-                        split_line = line.split(' ')
-                        ground_truth = VALID_RESULTS[split_line[0]]
-                        image_results = np.array(split_line[1:], dtype=np.int)
-                        images_scores_sum += np.sum(np.abs(image_results - ground_truth)) / np.sum(ground_truth)
+        results_file_path: Path = student_output_directory / 'results.json'
+        try:
+            with results_file_path.open() as results_file:
+                results = json.load(results_file)
 
-                score = images_scores_sum / len(VALID_RESULTS) if number_of_lines else float('inf')
-                results[student_output_directory.name] = score
-            except Exception as e:
-                print(f'{student_output_directory.name} failed: {e}', file=sys.stderr)
+            images_scores_sum = 0.0
+            for image_name, valid_result in VALID_RESULTS.items():
+                image_result = results[image_name] if image_name in results else 0
+                image_result = image_result if len(image_result) == len(valid_result) else 0
+                images_scores_sum += np.sum(np.abs(image_result - valid_result)) / len(valid_result)
+
+            score = images_scores_sum / len(VALID_RESULTS)
+            results[student_output_directory.name] = score
+        except Exception as e:
+            print(f'{student_output_directory.name} failed: {e}', file=sys.stderr)
 
     return results
 
 
-def process_application_directory(path: Path, images_directory: Path, output_dir: Path) -> Tuple[str, str]:
+def process_application_directory(path: Path, input_file: Path, images_directory: Path,
+                                  output_dir: Path) -> Tuple[str, str]:
+    print(f'Processing "{path.name}"...')
+    requirements_file = path / 'requirements.txt'
+    if requirements_file.exists():
+        print('Installing external dependencies...')
+        temp_venv_dir = Path(tempfile.gettempdir()) / 'SiSWVenv'
+        venv_builder = venv.EnvBuilder(system_site_packages=True, clear=True, with_pip=True)
+        venv_builder.create(str(temp_venv_dir))
+        interpreter = str(temp_venv_dir / 'bin' / 'python')
+        try:
+            subprocess.check_call([interpreter, '-m', 'pip', '-qqq', 'install', '-r', str(requirements_file)])
+        except subprocess.CalledProcessError:
+            return path.name, 'PIPFAILED'
+    else:
+        interpreter = sys.executable
+
     for application_file in path.iterdir():
-        if application_file.name.endswith('.exe'):
-            student_name = application_file.name[:-4]
+        if application_file.name.endswith('.py'):
+            student_name = application_file.name[:-3]
             student_output_dir = output_dir / student_name
             student_output_dir.mkdir(exist_ok=True)
             results_file = student_output_dir / 'results.txt'
             stdout_file = student_output_dir / 'stdout'
             stderr_file = student_output_dir / 'stderr'
 
-            print(f'Running {student_name}...')
+            print(f'Running "{student_name}"...')
             try:
-                with open(stdout_file, 'w') as stdout, open(stderr_file, 'w') as stderr:
-                    subprocess.run([str(application_file), str(images_directory), str(results_file)],
-                                   cwd=str(path), stdout=stdout, stderr=stderr, timeout=100)
+                with stdout_file.open('w') as stdout, stderr_file.open('w') as stderr:
+                    subprocess.run([interpreter, str(application_file), str(images_directory), str(input_file),
+                                    str(results_file)], cwd=str(path), stdout=stdout, stderr=stderr, timeout=100,
+                                   env={'PYTHONPATH': str(path)})
             except subprocess.TimeoutExpired:
                 return student_name, 'TIMEOUT'
             except subprocess.SubprocessError:
@@ -105,7 +113,7 @@ def process_application_directory(path: Path, images_directory: Path, output_dir
 
             return student_name, 'OK'
 
-    return path.name, 'NOEXE'
+    return path.name, 'NOSCRIPT'
 
 
 if __name__ == '__main__':
